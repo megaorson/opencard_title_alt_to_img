5369d478093c34f72913bf68a490dd049aeb257a
Victor <dubinajjot@gmail.com>
2016-06-20 14:19:53 +0300

    finish with first version of plugin

---
+++ install.sql
@@ -0,0 +1,4 @@
+ALTER TABLE oc_product_description ADD COLUMN title_img VARCHAR(200);
+ALTER TABLE oc_product_description ADD COLUMN alt_img VARCHAR(200);
+ALTER TABLE oc_product_image ADD COLUMN title_img VARCHAR(200);
+ALTER TABLE oc_product_image ADD COLUMN alt_img VARCHAR(200);
