8df2dd0a604d76a728aad5d4b54925098952c7b7
orson <orson@orson-X550VB>
2016-06-22 20:48:07 +0300

    added title and alt for logo

--- install.sql
+++ install.sql
@@ -2,3 +2,5 @@ ALTER TABLE oc_product_description ADD COLUMN title_img VARCHAR(200);
 ALTER TABLE oc_product_description ADD COLUMN alt_img VARCHAR(200);
 ALTER TABLE oc_product_image ADD COLUMN title_img VARCHAR(200);
 ALTER TABLE oc_product_image ADD COLUMN alt_img VARCHAR(200);
+INSERT INTO oc_setting(store_id,code,key,value,serialized) VALUES(0,'config','config_title_image','',0);
+INSERT INTO oc_setting(store_id,code,key,value,serialized) VALUES(0,'config','config_alt_image','',0);
